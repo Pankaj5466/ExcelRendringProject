0226cf7a4340f6fb929ce5c1b9ff697b1c23a286
Pankaj Kumar <pankaj@Pankajs-MacBook-Air.local>
2022-10-02 00:26:27 +0530

    Added filecose + selectFile button + JSON div tag on HTML

--- app.js
+++ app.js
@@ -14,4 +14,30 @@ button.addEventListener('click',()=>{
         button.textContent = 'Switch to Dark Theme'
     else
         button.textContent = 'Switch To Light Theme'
+});
+
+
+const fileName = "Copy of CopyOf_avaScriptNots(CanBeSafelyDeleted).xlsx";
+let data=[{
+    "name":"jayanth",
+    "data":"scd",
+    "abc":"sdef"
+}]
+
+document.querySelector('.excelbutton').addEventListener('click',()=>{
+    XLSX.utils.json_to_sheet(data,'out.xlsx');
+
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(fileName);
+
+    fileName.onload = (event)=>{
+        let data = event.target.result;
+        let workbook  = XLSX.read(data,{type:'binary'});
+
+        workbook.SheetNames.forEach(sheet=>{
+            let rowObject = XLSX.sheet_to_row_object_array(workbook.Sheet[sheet]);
+
+            console.log(rowObject);
+        })
+    }
 })
